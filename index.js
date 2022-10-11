9e03b151724bcff4f8f04abac6a20be446ccf660
Hoang Tran <jashoang2583@gmail.com>
2022-10-11 15:02:52 -0700

    commit tree f41a4a0a3785c3a676893975361b97af88f51716

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+num1 * num2;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10)+ 1;
+
+const num3 = 4;
+const num4 = 32;
+const mod = num3 % num4;
+
+let max = Math.max(5,10, 15, 20);
